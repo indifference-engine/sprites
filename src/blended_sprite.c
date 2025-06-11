e4bae926aa95674edc13f6f64d64bdbd1a2a30e5
siliconspecter <siliconspecter@outlook.com>
2025-06-11 23:24:29 +0100

    Lint.

--- src/blended_sprite.c
+++ src/blended_sprite.c
@@ -1,58 +1,57 @@
 #include "blended_sprite.h"
 
-void blended_sprite(
-    const int sprite_rows,
-    const int sprite_columns,
-    const float *const sprite_opacities,
-    const float *const sprite_reds,
-    const float *const sprite_greens,
-    const float *const sprite_blues,
-    const float opacity,
-    const float red,
-    const float green,
-    const float blue,
-    const int row,
-    const int column,
-    const int viewport_rows,
-    const int viewport_columns,
-    float *const viewport_opacities,
-    float *const viewport_reds,
-    float *const viewport_greens,
-    float *const viewport_blues)
-{
+void blended_sprite(const int sprite_rows, const int sprite_columns,
+                    const float *const sprite_opacities,
+                    const float *const sprite_reds,
+                    const float *const sprite_greens,
+                    const float *const sprite_blues, const float opacity,
+                    const float red, const float green, const float blue,
+                    const int row, const int column, const int viewport_rows,
+                    const int viewport_columns, float *const viewport_opacities,
+                    float *const viewport_reds, float *const viewport_greens,
+                    float *const viewport_blues) {
   const int skipped_top_rows = row < 0 ? -row : 0;
-  const int skipped_bottom_rows = row + sprite_rows > viewport_rows ? row + sprite_rows - viewport_rows : 0;
+  const int skipped_bottom_rows =
+      row + sprite_rows > viewport_rows ? row + sprite_rows - viewport_rows : 0;
   const int visible_rows = sprite_rows - skipped_top_rows - skipped_bottom_rows;
 
-  if (visible_rows <= 0)
-  {
+  if (visible_rows <= 0) {
     return;
   }
 
   const int skipped_left_columns = column < 0 ? -column : 0;
-  const int skipped_right_columns = column + sprite_columns > viewport_columns ? column + sprite_columns - viewport_columns : 0;
-  const int visible_columns = sprite_columns - skipped_left_columns - skipped_right_columns;
+  const int skipped_right_columns =
+      column + sprite_columns > viewport_columns
+          ? column + sprite_columns - viewport_columns
+          : 0;
+  const int visible_columns =
+      sprite_columns - skipped_left_columns - skipped_right_columns;
 
-  if (visible_columns <= 0)
-  {
+  if (visible_columns <= 0) {
     return;
   }
 
   int sprite_index = skipped_top_rows * sprite_columns + skipped_left_columns;
   const int sprite_skip = sprite_columns - visible_columns;
-  int viewport_index = (row > 0 ? row * viewport_columns : 0) + (column > 0 ? column : 0);
+  int viewport_index =
+      (row > 0 ? row * viewport_columns : 0) + (column > 0 ? column : 0);
   const int viewport_skip = viewport_columns - visible_columns;
 
-  for (int row = 0; row < visible_rows; row++)
-  {
-    for (int column = 0; column < visible_columns; column++)
-    {
+  for (int row = 0; row < visible_rows; row++) {
+    for (int column = 0; column < visible_columns; column++) {
       const float forward_opacity = sprite_opacities[sprite_index] * opacity;
       const float inverse_opacity = 1.0f - forward_opacity;
-      viewport_opacities[viewport_index] = 1.0f - (1.0f - viewport_opacities[viewport_index]) * inverse_opacity;
-      viewport_reds[viewport_index] = viewport_reds[viewport_index] * inverse_opacity + red * sprite_reds[sprite_index] * forward_opacity;
-      viewport_greens[viewport_index] = viewport_greens[viewport_index] * inverse_opacity + green * sprite_greens[sprite_index] * forward_opacity;
-      viewport_blues[viewport_index] = viewport_blues[viewport_index] * inverse_opacity + blue * sprite_blues[sprite_index] * forward_opacity;
+      viewport_opacities[viewport_index] =
+          1.0f - (1.0f - viewport_opacities[viewport_index]) * inverse_opacity;
+      viewport_reds[viewport_index] =
+          viewport_reds[viewport_index] * inverse_opacity +
+          red * sprite_reds[sprite_index] * forward_opacity;
+      viewport_greens[viewport_index] =
+          viewport_greens[viewport_index] * inverse_opacity +
+          green * sprite_greens[sprite_index] * forward_opacity;
+      viewport_blues[viewport_index] =
+          viewport_blues[viewport_index] * inverse_opacity +
+          blue * sprite_blues[sprite_index] * forward_opacity;
 
       sprite_index++;
       viewport_index++;
